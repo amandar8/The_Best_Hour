66789e1d50e81da98d4bbc0eec357c2997535511
Amanda Rutherfoord <amandaruth08@gmail.com>
2018-07-09 15:44:54 -0500

    pages set up

--- public/js/script.js
+++ public/js/script.js
@@ -1,62 +1,409 @@
-'use strict'
+(function() {
 
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 8000;
-const users = require('./routes/users');
-
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const cookieParser = require('cookie-parser');
-
-// let assassins = require('./routes/assassinroutes');
-// let codeNames = require('./routes/codenamesroutes');
-// let targets = require('./routes/targetroutes');
-// let clients = require('./routes/clientroutes');
-// let contracts= require('./routes/contractroutes');
-// let assassinContracts = require('./routes/assassincontractroutes');
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.disable('x-powered-by');
-
-app.use(morgan('short'));
-app.use(bodyParser.json()); //allows us to use request.body to retrieve data from front-end
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(cookieParser());
-app.use(users);
+  function initMap() {
+    let currentLocation = {
+      lat: 30.272,
+      lng: -97.760
+    }
+    configureMap(currentLocation);
 
 
-app.use(express.static(path.join('public'))); //gives us access to public folder
+  }
+  /////////////////////////////////////////
 
-// app.use(assassins); //allows us to use our routes
-// app.use(codeNames);
-// app.use(targets);
-// app.use(clients);
-// app.use(contracts);
-// app.use(assassinContracts);
 
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
+  function configureMap(currentLocation) {
+    map = new google.maps.Map(document.getElementById('foodMap'), {
+      center: currentLocation,
+      zoom: 15,
+      styles: [{
+          stylers: [{
+            visibility: 'simplified',
+          }]
+        }, {
+          elementType: 'labels',
+          stylers: [{
+            visibility: 'off'
+          }]
+        }]
+        [{
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#1d2c4d"
+            }]
+          },
+          {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#8ec3b9"
+            }]
+          },
+          {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#1a3646"
+            }]
+          },
+          {
+            "featureType": "administrative.country",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+              "color": "#4b6878"
+            }]
+          },
+          {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#64779e"
+            }]
+          },
+          {
+            "featureType": "administrative.province",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+              "color": "#4b6878"
+            }]
+          },
+          {
+            "featureType": "landscape.man_made",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+              "color": "#334e87"
+            }]
+          },
+          {
+            "featureType": "landscape.natural",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#023e58"
+            }]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#283d6a"
+            }]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#6f9ba5"
+            }]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#1d2c4d"
+            }]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "geometry.fill",
+            "stylers": [{
+              "color": "#023e58"
+            }]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#3C7680"
+            }]
+          },
+          {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#304a7d"
+            }]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#98a5be"
+            }]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#1d2c4d"
+            }]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#2c6675"
+            }]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+              "color": "#255763"
+            }]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#b0d5ce"
+            }]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#023e58"
+            }]
+          },
+          {
+            "featureType": "transit",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#98a5be"
+            }]
+          },
+          {
+            "featureType": "transit",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#1d2c4d"
+            }]
+          },
+          {
+            "featureType": "transit.line",
+            "elementType": "geometry.fill",
+            "stylers": [{
+              "color": "#283d6a"
+            }]
+          },
+          {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#3a4762"
+            }]
+          },
+          {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#0e1626"
+            }]
+          },
+          {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#4e6d70"
+            }]
+          }
+        ]
+    });
+    infoWindow = new google.maps.InfoWindow();
+    service = new google.maps.places.PlacesService(map);
 
-app.use((err, req, res, next) => {
-  if (err.status) {
-    return res
-      .status(err.status)
-      .set('Content-Type', 'text/plain')
-      .send(err.message);
+    let input = document.getElementById('pac-input');
+    let autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    let infowindow = new google.maps.InfoWindow();
+    let infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+    let marker = new google.maps.Marker({
+      map: map,
+
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+
+    autocomplete.addListener('place_changed', function() {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+
+
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(15);
+      }
+
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+
+      infowindowContent.children['place-icon'].src = place.icon;
+      infowindowContent.children['place-name'].textContent = place.name;
+      infowindowContent.children['place-address'].textContent = address;
+      infowindow.open(map, marker);
+    });
+  }
+  ///////////////////////////////////////
+
+
+  let myMarkers = [];
+
+  function performSearch(searchTerm) {
+    console.log(searchTerm);
+    let request = {
+      bounds: map.getBounds(),
+      keyword: searchTerm
+    };
+    service.radarSearch(request, setMarkers);
+
   }
 
-  console.error(err.stack);
-  res.sendStatus(500);
-});
+  function setMarkers(results, status) {
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+      window.alert("Please choose another item or new location!");
+      console.error(status);
+      return;
+    }
+    for (let i = 0, result; result = results[i]; i++) {
+      addMarker(result);
 
-app.listen(port, function() {
-  console.log('Listening on port', port);
-});
 
-module.exports = app;
+    }
+  }
+  ///////////////////////////////////////
+  function randomSearch(searchTerm) {
+    console.log(searchTerm);
+    let request = {
+      bounds: map.getBounds(),
+      keyword: searchTerm
+    };
+
+    service.radarSearch(request, randomSetMarkers);
+  }
+
+  function randomSetMarkers(results, status) {
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+      window.alert("Please choose another item or new location!");
+      console.error(status);
+      return;
+    }
+    let randomItem = results[Math.floor(Math.random() * results.length)];
+    addMarker(randomItem);
+
+
+  }
+  /////////////////////////////////////////////////////
+
+  function addMarker(place) {
+    let marker = new google.maps.Marker({
+      map: map,
+      animation: google.maps.Animation.DROP,
+      position: place.geometry.location,
+      icon: {
+        url: 'https://image.flaticon.com/icons/svg/37/37481.svg',
+        anchor: new google.maps.Point(30, 30),
+        scaledSize: new google.maps.Size(25, 30)
+      }
+
+    });
+    myMarkers.push(marker);
+
+
+    google.maps.event.addListener(marker, 'click', function() {
+      service.getDetails(place, function(result, status) {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          console.error(status);
+          return;
+        }
+        infoWindow.setContent(result.name + '<div><strong>' +
+          result.formatted_address + '</div>' + result.website + '</div>' + " " + result.formatted_phone_number + '</div>');
+        infoWindow.open(map, marker);
+      });
+    });
+  }
+
+  function configureRestaurantOpenNow() {
+    function removeMarkers() {
+      for (i = 0; i < myMarkers.length; i++) {
+        myMarkers[i].setMap(null)
+      }
+    }
+    let openNowButton = document.getElementById('open-button');
+    openNowButton.addEventListener('click', function() {
+      removeMarkers();
+      performSearch("Restaurants open in");
+    });
+  }
+
+  function configureRestaurantButton() {
+    function removeMarkers() {
+      for (i = 0; i < myMarkers.length; i++) {
+        myMarkers[i].setMap(null)
+      }
+    }
+    let foodButton = document.getElementById('food-button');
+    foodButton.addEventListener('click', function() {
+      removeMarkers();
+      let foodSelectElement = $('#foodType option:selected');
+      performSearch(foodSelectElement.text() + " " + "in");
+    });
+  }
+
+
+  function configureRandomRestaurant() {
+    function removeMarkers() {
+      for (i = 0; i < myMarkers.length; i++) {
+        myMarkers[i].setMap(null);
+      }
+    }
+    let randomButton = document.getElementById('random-button');
+    randomButton.addEventListener('click', function() {
+      removeMarkers();
+      let foodSelectElement = $('#foodType option:selected');
+      randomSearch(foodSelectElement.text() + " " + "in");
+    });
+  }
+
+  function configureMealTime() {
+    function removeMarkers() {
+      for (i = 0; i < myMarkers.length; i++) {
+        myMarkers[i].setMap(null);
+      }
+    }
+    let mealButton = document.getElementById('rating-button');
+    mealButton.addEventListener('click', function() {
+      removeMarkers();
+      let ratingSelectElement = $('#starType option:selected');
+      performSearch(ratingSelectElement.text() + " " + "in");
+    });
+  }
+
+
+
+
+  initMap();
+  configureRestaurantButton();
+  configureRandomRestaurant();
+  configureMealTime();
+  configureRestaurantOpenNow();
+})();
